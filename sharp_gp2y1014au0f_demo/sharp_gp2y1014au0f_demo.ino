0777cbec4661b288fb46da5703c825c1d1dbeea5
igormesarec <43310884+igormesarec@users.noreply.github.com>
2019-01-06 16:00:41 +0100

    Dust density calculation with starting Voc
    Add:
    - the wiring sketch comment 
    - some more coments
    - Dust density calculation based on initial Voc value 
    - Serial print two Dust Density values (one based on sensor specs, one based on min recorder Voc during runtime)

--- sharp_gp2y1014au0f_demo/sharp_gp2y1014au0f_demo.ino
+++ sharp_gp2y1014au0f_demo/sharp_gp2y1014au0f_demo.ino
@@ -1,22 +1,67 @@
-/////////////////////////////////////////////////////////////////////////////
-// Sharp GP2Y1014AU0F Dust Sensor Demo
+////////////////////////////////////////////////////////////////////////////
+// Sharp GP2Y1014AU0F Dust Sensor
+//
+// Original code from "sharpsensoruser" on https://github.com/sharpsensoruser/ - NICE WORK
+// There is also a detailed description how the sensor work and some background on dust sensing https://github.com/sharpsensoruser/sharp-sensor-demos/wiki
+//
+// Modification to the program by "Igor Mesarec" Jan 2018
+// igormesarec/sharp-sensor-demos
+//
+// Connections on GP2Y1014
+// Pin 1 - starting from the pin closer to the sensor edge
+// Pin 1 - V-Led
+// Pin 2 - LED-GND
+// Pin 3 - LED
+// Pin 4 - S-GND
+// Pin 5 - Vo
+// Pin 6 - Vcc
 //
 // Board Connection:
 //   GP2Y1014    Arduino
-//   V-LED       Between R1 and C1
+//   V-LED       Between *R1 (150ohms) and **C1 (150uF) to 5v (arduino)
 //   LED-GND     C1 and GND
 //   LED         Pin 7
 //   S-GND       GND
 //   Vo          A5
 //   Vcc         5V
+// *R1 - is connected betwen V-LED and 5V
+// **C1 - is connected betwen Led-GND and V-LED
+//
+// connection scheme:
+// ──────────────────   GP1Y1014 Conector  ───────────────────
+// ┌────┬───────────┬─────┬──────┬─────┬─────┬──────┐
+//          1                    2         3           4         5          6
+//       V─LED               LED─GND     LED       S─GND       Vo        Vcc
+//          │                   │         │          │        │         │
+//          │                   │         │          │        │         │
+//          ├───(+)C1(─)───┤         │          │        │         │
+//          │      [220uF]      │         │          │        │         │
+//          │                   │         │          │        │         │
+//          R1                   │         │          │        │         │
+//      [150 ohms]               │         │          │        │         │
+//          │                   │         │          │        │         │
+//         5V                   GND      PIN 7       GND         A5         5V
+// └────┴───────────┴─────┴──────┴─────┴─────┴──────┘
+//  ──────────────────   ARDUINO (UNO)  ────────────────────
+//
+// Sensor information: dest sensing from 0 ug/m3 and 500 ug/m3
+// Voc is 0.6 Volts for dust free acordind sensor spec
+// sensitivity in units of V per 100ug/m3 (K = 0.5)--> 0.5 Volts/(100 ug/m3)
+//
+// LED Pulse Drive Circuit:
+// LED pulsed on once every 10ms, pulse duration or width of 0.32ms. 
+// Once the LED is turned on sample the resulting analog output voltage after 0.28ms   
 //
 // Serial monitor setting:
 //   9600 baud
 /////////////////////////////////////////////////////////////////////////////
 
+
+// IGOR - dodaj da računa stanje tudi za o,6V Voc - in potem računa actual value 
+
 // Choose program options.
-//#define PRINT_RAW_DATA
-#define USE_AVG
+//#define PRINT_RAW_DATA  //uncomet if you wont raw data displayed in serial monitor
+#define USE_AVG   //calculate average value in N mesared values
 
 // Arduino pin numbers.
 const int sharpLEDPin = 7;   // Arduino digital pin 7 connect to sensor LED.
@@ -30,7 +75,8 @@ static int VoRawCount = 0;
 #endif // USE_AVG
 
 // Set the typical output voltage in Volts when there is zero dust. 
-static float Voc = 0.6;
+static float Voc = 0.6;  //Voc is 0.6 Volts for dust free acordind sensor spec
+static float VocT = 0.6;  //Self calibration overvrite the Coc with minimum measered value - this variable you need to have the treshold for the sensor  (facturi calibration)
 
 // Use the typical sensitivity in units of V per 100ug/m3.
 const float K = 0.5;
@@ -54,6 +100,9 @@ void printFValue(String text, float value, String units, bool isLast = false) {
   if (!isLast) {
     Serial.print(", ");
   }
+    else {
+    Serial.println();
+  }
 }
 
 /////////////////////////////////////////////////////////////////////////////
@@ -71,6 +120,7 @@ void setup() {
   Serial.println("");
   Serial.println("GP2Y1014AU0F Demo");
   Serial.println("=================");
+  VocT = Voc; //corect the VocT (to have alwas data for the initioal Voc
 }
 
 // Arduino main loop.
@@ -113,16 +163,39 @@ void loop() {
   // Compute the output voltage in Volts.
   Vo = Vo / 1024.0 * 5.0;
   printFValue("Vo", Vo*1000.0, "mV");
+  printFValue("Voc", Voc*1000.0, "mV"); // Serial print the runtime min value for the Voc - minimal measered value of the Voc during the runtime - this is 0 dust
 
-  // Convert to Dust Density in units of ug/m3.
-  float dV = Vo - Voc;
+  //------------ Dust density calculated by Voc set in the begining - like in sensor spec  ------------
+  // Calculate Dust Density in units of ug/m3 for the initial Voc (as in sensor specs)
+  float dV = Vo - VocT;
+  if ( dV < 0 ) {
+    dV = 0;
+  }
+  float dustDensity = dV / K * 100.0;
+  String SPT1 = ""; // add this to serial print the set Voc
+  SPT1 = ("Dust Density(Voc ");
+  SPT1 += VocT;
+  SPT1 += (" V)");
+  
+  printFValue(SPT1, dustDensity, "ug/m3", false); // Print the values
+  dV = 0; // Reset the dV to 0 for the next calculation
+  dustDensity = 0; // Reset the dustDensity to 0 for the next calculation
+  
+  //------------ Dust density calculated by lowest output voltage during runtime  ------------
+  // Convert to Dust Density in units of ug/m3. 
+  // During runtime, the Voc value is updated dynamically whenever a lower output voltage is sense. 
+  // this cover the dust sensing below the sensors specefied range from 0 to Voc (0,6V) as specified in the beginin
+  dV = Vo - Voc;
   if ( dV < 0 ) {
     dV = 0;
     Voc = Vo;
   }
-  float dustDensity = dV / K * 100.0;
-  printFValue("DustDensity", dustDensity, "ug/m3", true);
+  dustDensity = dV / K * 100.0;
+  SPT1 = "";
+  SPT1 = ("Dust Density(Voc ");
+  SPT1 += Voc;
+  SPT1 += (" V)");
+  printFValue(SPT1, dustDensity, "ug/m3", true);
   Serial.println("");
   
-} // END PROGRAM
-
+} 
